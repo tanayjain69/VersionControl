0010186d68bd8f38d7032d3edd73f61980e42f83
Tanay Jain <tanayjain1206@gmail.com>
2025-09-11 20:57:53 +0530

    Improved Efficiency a LOT

--- VersionControl.cpp
+++ VersionControl.cpp
@@ -1,14 +1,17 @@
-#include<iostream>
-#include<ctime>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
 // Constants
 const int SIZE = 1e3;
 
-// Nodes for the Tree Class
-class TreeNode{
+class Tree;
+
+// Node for the Tree Class
+class TreeNode {
 public:
     int version_id;
     string content;
@@ -38,72 +41,60 @@ public:
     }
 };
 
-// Custom hashmap that works for both integers and strings
+
 template <typename T1, typename T2>
 class HashMap {
 private:
+    vector<vector<pair<T1, T2>>> table;
+
     int hash_fn(int key) {
-        return key%SIZE;
+        return key % SIZE;
     }
 
-    int hash_fn(string key) {
-        int x=0;
-        for (char c:key) x+= (c-'\0');
-        return x%SIZE;
+    int hash_fn(const string& key) {
+        int x = 0;
+        for (char c : key) x += c;
+        return x % SIZE;
     }
 
 public:
-    vector<T1> keys;
-    vector<T2> nodes;
-    T1 init_val;
-
-    HashMap(T1 initial_val) {
-        this->keys = vector<T1>(SIZE, initial_val);
-        this->nodes = vector<T2>(SIZE, nullptr);
-        init_val=initial_val;
+    HashMap() {
+        table.resize(SIZE);
     }
 
+    ~HashMap() {}
+
     void insert(T1 key, T2 val) {
-        int hashed = hash_fn(key);
-        if (keys[hashed] == init_val) {
-            keys[hashed] = key;
-            nodes[hashed] = val;
-        } else {
-            // Full round
-            int tph = hashed;
-            tph = (tph+1)%SIZE;
-            while ((tph!=hashed) && keys[tph]!=init_val) {
-                tph = (tph+1)%SIZE;
-            }
-            if (tph==hashed) cout<<"HASHMAP FULL"<<'\n';
-            else {
-                keys[tph] = key;
-                nodes[tph] = val;
+        int index = hash_fn(key);
+
+        for (auto& pair : table[index]) {
+            if (pair.first == key) {
+                pair.second = val;
+                return;
             }
         }
+        table[index].push_back({key, val});
     }
 
     T2 get(T1 key) {
-        int hashed = hash_fn(key);
-        int temp = hashed;
-        while (keys[hashed]!=key) {hashed = (hashed+1)%SIZE; if(hashed==temp) break;}
-        if (keys[hashed]==key) return nodes[hashed];
-        else return nullptr;
+        int index = hash_fn(key);
+        for (const auto& pair : table[index]) {
+            if (pair.first == key) {
+                return pair.second;
+            }
+        }
+        return nullptr;
     }
 
     bool delete_entry(T1 key) {
-        int hashed = hash_fn(key);
-        int temp = hashed;
-        while (keys[hashed]!=key) {
-            hashed = (hashed+1)%SIZE; 
-            if(hashed==temp) break;
+        int index = hash_fn(key);
+        for (auto it = table[index].begin(); it != table[index].end(); ++it) {
+            if (it->first == key) {
+                table[index].erase(it);
+                return true;
+            }
         }
-        if (keys[hashed]==key) {
-            keys[hashed] = init_val; 
-            delete nodes[hashed]; 
-            return true;
-        }
-        else return false;
+        return false;
     }
 };
 
@@ -112,15 +103,15 @@ class Tree {
 public:
     TreeNode* root;
     TreeNode* Active;
-    HashMap<int, TreeNode*> version_ctrl{-1};
+    HashMap<int, TreeNode*> version_ctrl;
     int total_versions;
     string file_name;
     vector<pair<time_t, int>> snapshotted_versions;
     time_t last_modified;
 
-    Tree (string filename) {
+    Tree(string filename) {
         file_name = filename;
-        TreeNode* file =  new TreeNode(0, "", "created file");
+        TreeNode* file = new TreeNode(0, "", "created file");
         root = file;
         Active = file;
         file->snapshot("Initial Snapshot");
@@ -144,47 +135,46 @@ public:
 
     void insert(string content) {
         if (Active->snapshot_taken) {
-            TreeNode* newnode = new TreeNode(total_versions, Active->content+content, "new version created");
+            TreeNode* newnode = new TreeNode(total_versions, Active->content + content, "new version created");
             Active->children.push_back(newnode);
             newnode->Parent = Active;
             Active = newnode;
             version_ctrl.insert(total_versions, newnode);
             total_versions++;
-
         } else {
-            Active->content += content; 
-            Active->last_modified=time(nullptr);
+            Active->content += content;
+            Active->last_modified = time(nullptr);
         }
         last_modified = Active->last_modified;
     }
 
     void update(string content) {
         if (Active->snapshot_taken) {
-            TreeNode* newnode  = new TreeNode(total_versions, content, "new version created");
+            TreeNode* newnode = new TreeNode(total_versions, content, "new version created");
             Active->children.push_back(newnode);
             newnode->Parent = Active;
             Active = newnode;
             version_ctrl.insert(total_versions, newnode);
             total_versions++;
-
         } else {
-            Active->content = content; 
-            Active->last_modified=time(nullptr);
+            Active->content = content;
+            Active->last_modified = time(nullptr);
         }
         last_modified = Active->last_modified;
     }
 
     bool rollback(int v_id) {
-        if (v_id==-1) {
+        if (v_id == -1) {
             if (Active->Parent) {
-                Active = Active->Parent; 
+                Active = Active->Parent;
                 return true;
             } else return false;
-        }
-        else {
+        } else {
             TreeNode* u = version_ctrl.get(v_id);
-            if (u==nullptr) {cout<<"No version with this version id"<<'\n'<<'\n'; return false;}
-            else {
+            if (u == nullptr) {
+                cout<<"No version with this version id"<<'\n'<<'\n';
+                return false;
+            } else {
                 Active = u;
                 return true;
             }
@@ -211,9 +201,9 @@ public:
 
     void history() {
         sort(snapshotted_versions.begin(), snapshotted_versions.end());
-        for (int i=snapshotted_versions.size()-1; i>=0; i--) {
+        for (int i = snapshotted_versions.size() - 1; i >= 0; i--) {
             TreeNode* v = version_ctrl.get(snapshotted_versions[i].second);
-            cout<<"File "<<i+1<<'\n';
+            cout<<"File "<<i + 1<<'\n';
             cout<<"File Version ID: "<<v->version_id<<'\n';
             cout<<"Snapshot Timestamp: "<<ctime(&(v->snapshot_timestamp));
             cout<<"Snapshot message: "<<v->message<<'\n';
@@ -222,29 +212,35 @@ public:
     }
 };
 
-// MaxHeap for tracking system wide analytics
+
 class MaxHeap {
+private:
     vector<int> heap;
-    vector<Tree*> v;
+    vector<Tree*> v; 
 
     void heapifyup(int idx) {
-        while ((idx>0) && (heap[(idx-1)/2]<heap[idx])) {
+        while (idx > 0 && heap[(idx - 1) / 2] < heap[idx]) {
             swap(heap[idx], heap[(idx - 1) / 2]);
             swap(v[idx], v[(idx - 1) / 2]);
-            idx=(idx-1)/2;
+            idx = (idx - 1) / 2;
         }
     }
 
     void heapifydown(int idx) {
         int n = heap.size();
         while (true) {
-            int l = 2*idx+1, r=2*idx+2, c=idx;
-            if ((l<n) && (heap[l]>heap[c])) c=l;
-            if ((r<n) && (heap[r]>heap[c])) c=r;
-            if (c == idx) break;
-            swap(heap[idx], heap[c]);
-            swap(v[idx], v[c]);
-            idx=c;
+            int left = 2 * idx + 1;
+            int right = 2 * idx + 2;
+            int largest = idx;
+
+            if (left < n && heap[left] > heap[largest]) largest = left;
+            if (right < n && heap[right] > heap[largest]) largest = right;
+
+            if (largest == idx) break;
+
+            swap(heap[idx], heap[largest]);
+            swap(v[idx], v[largest]);
+            idx = largest;
         }
     }
 
@@ -252,28 +248,35 @@ public:
     void insert(int val, Tree* file) {
         heap.push_back(val);
         v.push_back(file);
-        heapifyup(heap.size()-1);
+        heapifyup(heap.size() - 1);
     }
-
-    // Specifically made to build heap in O(n) time
-    void buildHeap(const vector<Tree*>& elements, bool byVersions = false) {
-        heap.clear();
-        v.clear();
-        for (Tree* t : elements) {
-            if (byVersions)
-                heap.push_back(t->total_versions);
-            else
-                heap.push_back((int)t->last_modified);
-            v.push_back(t);
+    
+    void update(Tree* file, int new_priority) {
+        int index_to_update = -1;
+        for (int i = 0; i < v.size(); ++i) {
+            if (v[i] == file) {
+                index_to_update = i;
+                break;
+            }
         }
-        // bottom-up heapify (minizes operations)
-        for (int i = (heap.size()/2) - 1; i >= 0; i--) {
-            heapifydown(i);
+
+        if (index_to_update != -1) {
+            int old_priority = heap[index_to_update];
+            heap[index_to_update] = new_priority;
+
+            if (new_priority > old_priority) {
+                heapifyup(index_to_update);
+            } else {
+                heapifydown(index_to_update);
+            }
         }
     }
 
     int getMax() {
-        if (heap.empty()) cout<<"Empty!!"<<'\n';
+        if (heap.empty()) {
+            cout<<"Empty!!"<<'\n';
+            return -1;
+        }
         return heap[0];
     }
 
@@ -284,7 +287,10 @@ public:
         heap[0] = heap.back();
         heap.pop_back();
         v.pop_back();
-        if (!heap.empty()) heapifydown(0);
+
+        if (!heap.empty()) {
+            heapifydown(0);
+        }
         return maxVal;
     }
 };
@@ -293,7 +299,7 @@ public:
 // Mainloop
 int main() {
     vector<Tree*> ALL_FILES;
-    HashMap<string, Tree*> FileMap{""};
+    HashMap<string, Tree*> FileMap;
     MaxHeap recent_edits;
     MaxHeap most_versions;
     string inp;
@@ -306,24 +312,32 @@ int main() {
         string a;
 
         // Break input command words
-        for (char c:inp) {
-            if (c==' ' && v.size()<2) {
+        for (char c : inp) {
+            if (c == ' ' && v.size() < 2) {
                 v.push_back(a);
-                a="";
-            } else a+=c;
+                a = "";
+            } else a += c;
         }
         v.push_back(a);
 
         // Create a new file
         if (v[0] == "CREATE") {
-            if (v.size()>2) {
-                cout<<"Spaces are not allowed in the filename"<<'\n'<<'\n'; 
+            if (v.size() > 2) {
+                cout<<"Spaces are not allowed in the filename"<<'\n'<<'\n';
                 continue;
             }
-            Tree* newfile = new Tree(v[1]);
-            ALL_FILES.push_back(newfile);
-            FileMap.insert(v[1], newfile);
-            cout<<"New file "<<v[1]<<" created."<<'\n'<<'\n';
+            Tree* q = FileMap.get(v[1]);
+            if (!q) {
+                Tree* newfile = new Tree(v[1]);
+                ALL_FILES.push_back(newfile);
+                FileMap.insert(v[1], newfile);
+                
+                // Add the new file to the heaps
+                recent_edits.insert((int)newfile->last_modified, newfile);
+                most_versions.insert(newfile->total_versions, newfile);
+                
+                cout<<"New file "<<v[1]<<" created."<<'\n'<<'\n';
+            } else cout<<"File with this name already exists, try another name"<<'\n'<<'\n';
         }
 
         // Read a file
@@ -332,8 +346,7 @@ int main() {
             if (q) {
                 cout<<"Here are the contents of the Active version of the file "<<v[1]<<'\n';
                 cout<<q->Active->content<<'\n'<<'\n';
-            }
-            else cout<<"Invalid filename"<<'\n'<<'\n';
+            } else cout<<"Invalid filename"<<'\n'<<'\n';
         }
 
         // See all the snapshotted versions of a file
@@ -343,86 +356,121 @@ int main() {
                 if (q) {
                     cout<<"Here are all the snapshotted versions of the file "<<v[1]<<'\n'<<'\n';
                     q->history();
-                }
-                else cout<<"Invalid filename"<<'\n'<<'\n';
-            } else {cout<<"Invalid Command"<<'\n'<<'\n'; continue;}
+                } else cout<<"Invalid filename"<<'\n'<<'\n';
+            } else {
+                cout<<"Invalid Command"<<'\n'<<'\n';
+                continue;
+            }
         }
 
         // Insert content in a file (append)
         else if (v[0] == "INSERT") {
-            if (v.size()==3) {
+            if (v.size() == 3) {
                 Tree* q = FileMap.get(v[1]);
                 if (q) {
-                    q->insert(v[2]); 
+                    q->insert(v[2]);
+                    // Update heaps after modification
+                    recent_edits.update(q, (int)q->last_modified);
+                    most_versions.update(q, q->total_versions);
                     cout<<"Content successfully inserted in the "<<v[1]<<" file."<<'\n'<<'\n';
-                }
-                else cout<<"Invalid filename"<<'\n'<<'\n';
-            } else {cout<<"Invalid Command"<<'\n'<<'\n'; continue;}
+                } else cout<<"Invalid filename"<<'\n'<<'\n';
+            } else {
+                cout<<"Invalid Command"<<'\n'<<'\n';
+                continue;
+            }
         }
 
         // Update the content in the file
         else if (v[0] == "UPDATE") {
-            if (v.size()==3) {
+            if (v.size() == 3) {
                 Tree* q = FileMap.get(v[1]);
                 if (q) {
-                    q->update(v[2]); 
+                    q->update(v[2]);
+
+                    // Update heaps after modification
+                    recent_edits.update(q, (int)q->last_modified);
+                    most_versions.update(q, q->total_versions);
                     cout<<"Content for "<<v[1]<<" successfully updated."<<'\n'<<'\n';
-                }
-                else cout<<"Invalid filename"<<'\n'<<'\n';
-            } else {cout<<"Invalid Command"<<'\n'<<'\n'; continue;}
+                } else cout<<"Invalid filename"<<'\n'<<'\n';
+            } else {
+                cout<<"Invalid Command"<<'\n'<<'\n';
+                continue;
+            }
         }
 
         // Snapshot the Active version of the file
         else if (v[0] == "SNAPSHOT") {
-            if (v.size()==3) {
+            if (v.size() == 3) {
                 Tree* q = FileMap.get(v[1]);
-                if (q) q->snapshot(v[2]);
-                else cout<<"Invalid filename"<<'\n'<<'\n';
-            } else {cout<<"Invalid Command"<<'\n'<<'\n'; continue;}
-        }
-        
-        // Rollback to a particular version of a file
-        else if (v[0] == "ROLLBACK") {
-            if (v.size()==2 || v.size()==3) {
-                Tree* k = FileMap.get(v[1]);
-                if (k->rollback(((v.size()==3)?stoi(v[2]):-1))) cout<<"Successfully rolled back to version ID "<<k->Active->version_id<<" for the file "<<v[1]<<'\n'<<'\n';
-                else cout<<"Invalid filename or version-id"<<'\n'<<'\n';
-            } else {cout<<"Invalid Command"<<'\n'<<'\n'; continue;}
-        }
-        
-        //File deletion
-        else if (v[0] == "DELETE") {
-            if (v.size()==2) { 
-                if (FileMap.delete_entry(v[1])) {
-                    for (int i=0; i<((int)ALL_FILES.size()); i++) if(ALL_FILES[i]->file_name == v[1]) ALL_FILES.erase(ALL_FILES.begin()+i);
-                    cout<<"File deleted"<<'\n'<<'\n';
+                if (q) {
+                    if(q->snapshot(v[2])) {
+                         recent_edits.update(q, (int)q->last_modified);
+                    }
                 }
                 else cout<<"Invalid filename"<<'\n'<<'\n';
-            } else {cout<<"Invalid Command"<<'\n'<<'\n'; continue;}
+            } else {
+                cout<<"Invalid Command"<<'\n'<<'\n';
+                continue;
+            }
+        }
+
+        // Rollback to a particular version of a file
+        else if (v[0] == "ROLLBACK") {
+            if (v.size() == 2 || v.size() == 3) {
+                Tree* k = FileMap.get(v[1]);
+                if (k && k->rollback(((v.size() == 3) ? stoi(v[2]) : -1))) {
+                     cout<<"Successfully rolled back to version ID "<<k->Active->version_id<<" for the file "<<v[1]<<'\n'<<'\n';
+                }
+                else cout<<"Invalid filename or version-id"<<'\n'<<'\n';
+            } else {
+                cout<<"Invalid Command"<<'\n'<<'\n';
+                continue;
+            }
+        }
+
+        //File deletion
+        else if (v[0] == "DELETE") {
+            if (v.size() == 2) {
+                if (FileMap.delete_entry(v[1])) {
+                    for (int i = 0; i < ((int)ALL_FILES.size()); i++) {
+                        if (ALL_FILES[i]->file_name == v[1]) {
+                             delete ALL_FILES[i];
+                             ALL_FILES.erase(ALL_FILES.begin() + i);
+                             break;
+                        }
+                    }
+                    cout<<"File deleted"<<'\n'<<'\n';
+                } else cout<<"Invalid filename"<<'\n'<<'\n';
+            } else {
+                cout<<"Invalid Command"<<'\n'<<'\n';
+                continue;
+            }
         }
 
         // Check the recently edited files
         else if (v[0] == "RECENT_FILES") {
-            recent_edits.buildHeap(ALL_FILES, false);
+            MaxHeap temp_heap = recent_edits;
             cout<<"Latest Modified files- "<<'\n';
             int num_files = stoi(v[1]);
-            for (int i=0; i<min((int)ALL_FILES.size(), num_files); i++) {
-                Tree* m = recent_edits.removeTop();
-                if (m) cout<<m->file_name<<'\n'<<'\n';
+            for (int i = 0; i < min((int)ALL_FILES.size(), num_files); i++) {
+                Tree* m = temp_heap.removeTop();
+                if (m) cout<<m->file_name<<'\n';
                 else break;
             }
+             cout<<'\n';
         }
 
         // Checked the files with the maximum number of versions
         else if (v[0] == "BIGGEST_TREES") {
-            most_versions.buildHeap(ALL_FILES, true);
+            MaxHeap temp_heap = most_versions;
             cout<<"Largest Tree files-"<<'\n';
             int num_files = stoi(v[1]);
-            for (int i=0; i<num_files; i++) {
-                Tree* m = most_versions.removeTop();
-                if (m) cout<<m->file_name<<'\n'<<'\n';
+            for (int i = 0; i < min((int)ALL_FILES.size(), num_files); i++) {
+                Tree* m = temp_heap.removeTop();
+                if (m) cout<<m->file_name<<'\n';
                 else break;
             }
+             cout<<'\n';
         }
 
         // Type help to see the available commands with their syntaxes
@@ -434,6 +482,7 @@ int main() {
             cout<<"UPDATE <filename> <content>"<<'\n';
             cout<<"SNAPSHOT <filename> <message>"<<'\n';
             cout<<"ROLLBACK <filename> <version-id>"<<'\n';
+            cout<<"ROLLBACK <filename> (to previous version)"<<'\n';
             cout<<"HISTORY <filename>"<<'\n';
             cout<<"DELETE <filename>"<<'\n';
             cout<<"RECENT_FILES <number>"<<'\n';
@@ -443,12 +492,8 @@ int main() {
         }
 
         // End command
-        else if (v[0] =="STOP") {
-
-            // Remove all the files to unclog the dynamically allotted memory
-            for (int i=0; i<(int)(ALL_FILES.size()); i++) {
-                delete ALL_FILES[i];
-            }
+        else if (v[0] == "STOP") {
+            ALL_FILES.clear();
             break;
         }
 
